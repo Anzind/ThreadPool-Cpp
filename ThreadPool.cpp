40ad61adbb0d3ce1bc95789e59a6753fb4a03721
尔格林 <956371505@qq.com>
2024-08-13 21:14:26 +0800

    完成工作线程任务和管理者线程任务

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,1 +1,86 @@
-#include "ThreadPool.h"
+﻿#include "ThreadPool.h"
+#include <iostream>
+
+const int number = 2;
+
+ThreadPool::ThreadPool(int min, int max) :minThread(min), maxThread(max),
+shutdown(false), curThread(min), idleThread(min), exitThread(0) {
+	/* 创建管理者线程 */
+	t_manager = new thread(&ThreadPool::manager, this);
+	/* 创建工作线程 */
+	for (int i = 0; i < min; i++) {
+		//t_workers.emplace_back(thread(&ThreadPool::worker, this));
+		thread t(&ThreadPool::worker, this);
+		t_workers.insert(make_pair(t.get_id(), move(t)));
+	}
+}
+
+void ThreadPool::addTask(function<void(void)> task) {
+	{
+		lock_guard<mutex> locker(queueMutex);
+		taskQ.emplace(task);
+	}
+	condition.notify_one();
+}
+
+void ThreadPool::manager() {
+	while (!shutdown.load()) {
+		this_thread::sleep_for(chrono::seconds(3));
+		int idle = idleThread.load();
+		int cur = curThread.load();
+
+		if (idle > cur / 2 && cur > minThread) {
+			/* 若空闲线程>当前线程/2，则释放线程 */
+			exitThread.store(number);
+			condition.notify_all();
+
+			lock_guard<mutex> idsLocker(idsMutex);
+			for (auto id : t_workerIds) {
+				auto it = t_workers.find(id);
+				if (it != t_workers.end()) {
+					(*it).second.join();
+					t_workers.erase(it);
+				}
+			}
+			t_workerIds.clear();
+		}
+		else if (idle == 0 && cur < maxThread) {
+			/* 若空闲线程为0，则添加线程 */
+			//t_workers.emplace_back(thread(&ThreadPool::worker, this));
+			thread t(&ThreadPool::worker, this);
+			t_workers.insert(make_pair(t.get_id(), move(t)));
+			curThread++;
+			idleThread++;
+		}
+	}
+}
+
+void ThreadPool::worker() {
+	while (!shutdown.load()) {
+		function<void(void)> task = nullptr;
+		{
+			unique_lock<mutex> locker(queueMutex);
+			while (taskQ.empty() && shutdown) {
+				condition.wait(locker);
+				/* 唤醒后需要退出 */
+				if (exitThread > 0) {
+					curThread--;
+					exitThread--;
+					cout << "-------线程退出，ID:" << this_thread::get_id() << endl;
+					lock_guard<mutex> idsLocker(idsMutex);
+					t_workerIds.push_back(this_thread::get_id());
+					return;
+				}
+			}
+			if (!taskQ.empty()) {
+				task = move(taskQ.front());
+				taskQ.pop();
+			}
+		}
+		if (task) {
+			idleThread--;
+			task();
+			idleThread++;
+		}
+	}
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -1,5 +1,57 @@
-#pragma once
-class ThreadPool{
+﻿#pragma once
+#include <thread>
+#include <queue>
+#include <vector>
+#include <map>
+#include <atomic>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
 
+using namespace std;
+
+/*
+* 构成：
+* 1.管理者线程 -> 子线程，1个
+*		- 控制工作线程的数量：增加或减少
+* 2.若干工作线程 -> 子线程，n个
+*		- 从任务队列中取任务，并处理
+*		- 任务队列为空，则阻塞（条件变量处理）
+*		- 线程同步（互斥锁）
+*		- 当前数量，空闲的线程数量
+*		- 最小，最大线程数量
+* 3.任务队列 -> stl->queue
+*		- 互斥锁
+*		- 条件变量
+* 4.线程池开关 -> shutdown(bool)
+*/
+class ThreadPool {
+public:
+	ThreadPool(int min = 3, int max = thread::hardware_concurrency());
+	~ThreadPool();
+
+	/* 添加任务 */
+	void addTask(function<void(void)>);
+
+private:
+	void manager();
+	void worker();
+
+private:
+	thread* t_manager;
+	/* 存储已经退出了任务函数的线程ID */
+	vector<thread::id> t_workerIds;
+	map<thread::id, thread> t_workers;
+	atomic<int> minThread;
+	atomic<int> maxThread;
+	atomic<int> curThread;
+	atomic<int> idleThread;
+	atomic<int> exitThread;
+	atomic<bool> shutdown;
+
+	queue<function<void(void)>> taskQ;
+	mutex queueMutex;
+	mutex idsMutex;
+	condition_variable condition;
 };
 
